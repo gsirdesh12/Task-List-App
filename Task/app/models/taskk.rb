9bf93e40710cfd672bd2fb7b239e6567fa3a98ad
Jonathan Arp <jarp@nd.edu>
2016-12-04 21:30:25 -0500

    notes

--- Task/app/models/taskk.rb
+++ Task/app/models/taskk.rb
@@ -1,3 +1,6 @@
+# jarp: one of the amazing things about rails is you can have typos and the scaffolder will still work.
+#       you will probably want to redo this model and fix the Taskk
+
 class Taskk < ActiveRecord::Base
   belongs_to :task_list
 
