baae2a139a3b481b44433d4ba8b138cfdb7c5916
Jonathan Arp <jarp@nd.edu>
2016-12-16 16:20:25 -0500

    notes

--- Task/app/controllers/login_controller.rb
+++ Task/app/controllers/login_controller.rb
@@ -4,10 +4,13 @@ class LoginController < ApplicationController
   end
 
   def authenticate
+    # jarp: you are miss-matching username and email (from the form)
     @user = User.find_by username: params[:username], password: params[:password]
-
+    puts "user is #{@user}"
     if @user
       flash[:message] = "You have been logged in"
+      puts "login in user"
+      # jarp: user_id does not exist. all models have an .id
       session[:user_id] = @user.user_id
       redirect_to home_path
     else
